cc5dc1f0604740293559d9ba12e0f0c5f0d156a2
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2022-09-29 17:34:30 +0700

    Update worms_GO.R

--- worms_GO.R
+++ worms_GO.R
@@ -10,13 +10,13 @@ write.csv(gene, "/home/kaliki_sci/Hamsters_transcriptome/worms_time/OF_sig_genes
 gene <- gene$entrez
 
 ego <- enrichGO(gene          = gene,
-+                 universe      = names(geneList),
-+                 OrgDb         = org.Mm.eg.db,
-+                 ont           = "BP",       
-+                 pAdjustMethod = "BH",
-+                 pvalueCutoff  = 0.05,
-+                 qvalueCutoff  = 0.1,
-+                 readable      = TRUE)
+universe      = names(geneList),
+OrgDb         = org.Mm.eg.db,
+ont           = "BP",       
+pAdjustMethod = "BH",
+pvalueCutoff  = 0.05,
+qvalueCutoff  = 0.1,
+readable      = TRUE)
 go_df <- as.data.frame(ego@result)
 
 go_df <- distinct(go_df, geneID, .keep_all = TRUE)
@@ -26,15 +26,15 @@ des <- rev(des)
 go_df_20$Description <- factor(go_df_20$Description, levels = go_df_20$Description[order(-(go_df_20$pvalue))])
 png(filename="/home/kaliki_sci/Hamsters_transcriptome/worms_time/OF_GO_plot_l2fc2_BP.png", width=8, height=6, units="in", res=300)
 ggplot(go_df_20, aes(Description, Count, fill=p.adjust)) + 
-+     geom_col() +
-+     coord_flip() +
-+     scale_x_discrete(labels = str_wrap(des, width = 60)) +
-+     scale_fill_viridis() +
-+     ylab("Number Enriched") +
-+     xlab("GO Term") +
-+     theme(
-+         legend.position = "right",
-+         text = element_text(size = 10),
-+         axis.title.x = element_text(size = 10), 
-+         axis.title.y = element_text(size = 10))
+geom_col() +
+coord_flip() +
+scale_x_discrete(labels = str_wrap(des, width = 60)) +
+scale_fill_viridis() +
+ylab("Number Enriched") +
+xlab("GO Term") +
+theme(
+legend.position = "right",
+text = element_text(size = 10),
+axis.title.x = element_text(size = 10), 
+axis.title.y = element_text(size = 10))
 > dev.off()
