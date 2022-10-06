2f0b1516cde779407f34c11e903a38a66d451b63
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2022-10-06 18:25:46 +0700

    Update Script_different.R

--- Script_different.R
+++ Script_different.R
@@ -1,2 +1,5 @@
-
+# тепловая карта для всех генов
 heatmap.2(counts_norm, margins = c(6,15), scale="row", trace="none", Colv = NA, Rowv = NA, dendrogram = "none",col = redgreen(75), density.info="none", cexRow=0.6)
+# тепловая карта для 30 генов с наибольшим изменением Z-score
+topVarGenes <- head(order(rowVars(counts_norm), decreasing=TRUE), 30)
+heatmap.2(counts_norm[topVarGenes,], margins = c(6,15), scale="row", trace="none", Colv = NA, Rowv = NA, dendrogram = "none",col = redgreen(75), density.info="none", cexRow=1)
