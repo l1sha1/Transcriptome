756eb18108d0680d16ba4fccc3f7e8e7226df724
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2022-11-16 17:11:36 +0700

    Update Script_different.R

--- Script_different.R
+++ Script_different.R
@@ -3,3 +3,9 @@ heatmap.2(counts_norm, margins = c(6,15), scale="row", trace="none", Colv = NA, 
 # тепловая карта для 30 генов с наибольшим изменением Z-score
 topVarGenes <- head(order(rowVars(counts_norm), decreasing=TRUE), 30)
 heatmap.2(counts_norm[topVarGenes,], margins = c(6,15), scale="row", trace="none", Colv = NA, Rowv = NA, dendrogram = "none",col = redgreen(75), density.info="none", cexRow=1)
+# dotplot gene-group
+data_bp %>% 
+     filter(data_bp$norm_gene_expression > 0) %>%
+     ggplot(aes(x=group, y = cluster, size = norm_gene_expression)) + 
+     theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) +
+     geom_point() 
