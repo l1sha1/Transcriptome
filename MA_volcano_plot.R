40359e97a0a361eee2491c33fffdf0813432835a
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2024-03-07 16:31:38 +0700

    Update MA_volcano_plot.R

--- MA_volcano_plot.R
+++ MA_volcano_plot.R
@@ -10,3 +10,17 @@ CS$significant <- ifelse(CS$padj < .05 & abs(CS$log2FoldChange) > 1 , "Significa
 ggplot(CS, aes(log2FoldChange, -log10(padj), colour=significant))+ geom_point(size=1) + scale_y_continuous(limits=c(0, 15), oob=squish)+  scale_x_continuous(limits=c(-9, 10), oob=squish)
 
 ggplot(CS, aes(baseMean, log2FoldChange, colour=significant)) + geom_point(size=1) + scale_y_continuous(limits=c(-10, 10), oob=squish) + scale_x_log10() + geom_hline(yintercept = 0, colour="tomato1", size=1) + labs(x="mean of normalized counts", y="log fold change") + scale_colour_manual(name="q-value", values=("Significant"="red"), na.value="grey50") + theme_bw()
+
+
+
+# esp_control <- results(dds, contrast=list(c("Condition_esp_vs_control")))
+# esp <- as.data.frame(esp_control)
+esp$significant <- "NO"
+esp$significant[esp$log2FoldChange > 0.6 & esp$padj < 0.05] <- "UP"
+esp$significant[esp$log2FoldChange < -0.6 & esp$padj < 0.05] <- "DOWN"
+ggplot(data = esp, aes(x = log2FoldChange, y = -log10(padj), col = significant)) +
+    geom_vline(xintercept = c(-0.6, 0.6), col = "gray", linetype = 'dashed') +
+    geom_hline(yintercept = -log10(0.05), col = "gray", linetype = 'dashed') +
+    geom_point(size = 1) + scale_y_continuous(limits=c(0, 40), oob=squish) +  scale_x_continuous(limits=c(-5, 5), oob=squish) +
+    scale_color_manual(values = c("#FF0000", "grey", "#00CC00"),
+                       labels = c("Downregulated", "Not significant", "Upregulated")) 
