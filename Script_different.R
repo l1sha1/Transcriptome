35aff0ec02427152e8250c13d3588a8ce289bc1c
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2024-10-04 19:54:21 +0700

    Update Script_different.R

--- Script_different.R
+++ Script_different.R
@@ -2,6 +2,10 @@
 heatmap.2(counts_norm, margins = c(6,15), scale="row", trace="none", Colv = NA, Rowv = NA, dendrogram = "none",col = redgreen(75), density.info="none", cexRow=0.6)
 # тепловая карта для 30 генов с наибольшим изменением Z-score
 topVarGenes <- head(order(rowVars(counts_norm), decreasing=TRUE), 30)
+
+pheatmap(CC, scale = "row", fontsize_row = 5, fontsize_col = 7, cluster_rows = TRUE, cluster_cols = FALSE, legend = TRUE)
+
+
 heatmap.2(counts_norm[topVarGenes,], margins = c(6,15), scale="row", trace="none", Colv = NA, Rowv = NA, dendrogram = "none",col = redgreen(75), density.info="none", cexRow=1)
 # dotplot gene-group
 data_bp %>% 
